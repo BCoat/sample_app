96ce24098c13430965f1dca683fa918cac7cdbfb
Coatanea Brendan <coatanea.brendan@gmail.com>
2017-01-11 16:46:56 +0100

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,24 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @title = "S'inscrire"
+    @titre = "Inscription"
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Traite un succès d'enregistrement.
+      flash[:success] = "Bienvenue dans l'Application Exemple!"
+      redirect_to @user
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nom, :email, :password, :salt, :encrypted_password, :password_confirmation)
   end
 end
